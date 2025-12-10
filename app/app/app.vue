e39a905d896e206d8f920fb048b828faec19c427
Tom CZEKAJ <red57101@gmail.com>
2025-12-10 19:38:01 +0100

    :tada: Nuxt app setup

---
+++ app/app/app.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
+</template>
---
+++ app/app/pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+    <div>
+        <Button>Couscous</Button>
+    </div>
+</template>
+
+<style scoped></style>
