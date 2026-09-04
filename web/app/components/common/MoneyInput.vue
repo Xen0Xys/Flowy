b7f2a584216c17c88a0ab46e7c51a89c102e68ca
Tom CZEKAJ <red57101@gmail.com>
2026-09-04 22:48:20 +0200

    :bug: Normalize decimal separator input across locales in MoneyInput

--- web/app/components/common/MoneyInput.vue
+++ web/app/components/common/MoneyInput.vue
@@ -118,10 +118,13 @@ const onInput = (event: Event) => {
     const target = event.target as HTMLInputElement;
     const value = target.value;
     const decSep = decimalSeparator.value;
-    const allowedChars = props.allowNegative
-        ? new RegExp(`[^0-9\\${decSep}\\-]`, "g")
-        : new RegExp(`[^0-9\\${decSep}]`, "g");
+    const allowedChars = props.allowNegative ? /[^0-9.,\-]/g : /[^0-9.,]/g;
     let filtered = value.replace(allowedChars, "");
+    if (decSep === ",") {
+        filtered = filtered.replace(/\./g, ",");
+    } else {
+        filtered = filtered.replace(/,/g, ".");
+    }
 
     let sign = "";
     if (props.allowNegative) {
